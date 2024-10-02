4173dec20bdaace08df8bc0ecef20cc497816318
Márton Szűr <szurmarton@gmail.com>
2024-10-02 12:24:28 +0200

    Create new demo app

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
-import { share } from 'rxjs/operators';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app',
-    styleUrls: ['app.component.css'],
-    templateUrl: 'app.component.html'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    public number: number = 3000;
-    public observable: Observable<boolean>;
-    private observer: Observer<boolean>;
-
-    constructor() {
-        this.observable = new Observable<boolean>((observer: any) => this.observer = observer).pipe(share());
-
-        // For auto mode
-        setTimeout(() => this.number += this.number, 5000); // Update on 5 seconds
-    }
-
-    public trigger() {
-        this.observer.next(true);
-    }
+  title = 'demo';
 }
